4500afd0d1f881832d8188b42969c6e7e920c803
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2025-02-16 10:15:17 +0100

    fix index

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,4 @@
 import { Icons } from "@/components/icons";
-import { DataTableConfig } from "@/config/data-table";
-import { ColumnSort, Row } from "@tanstack/react-table";
 
 export interface NavItem {
   title: string;
@@ -36,41 +34,3 @@ export interface FooterItem {
 export type MainNavItem = NavItemWithOptionalChildren;
 
 export type SidebarNavItem = NavItemWithChildren;
-
-export interface DataTableRowAction<TData> {
-  row: Row<TData>;
-  type: "update" | "delete";
-}
-
-export interface DataTableFilterField<TData> {
-  id: StringKeyOf<TData>;
-  label: string;
-  placeholder?: string;
-  options?: Option[];
-}
-
-export interface DataTableAdvancedFilterField<TData>
-  extends DataTableFilterField<TData> {
-  type: ColumnType;
-}
-
-export interface Option {
-  label: string;
-  value: string;
-  icon?: React.ComponentType<{ className?: string }>;
-  count?: number;
-}
-
-export type StringKeyOf<TData> = Extract<keyof TData, string>;
-
-export type ColumnType = DataTableConfig["columnTypes"][number];
-
-export type ExtendedSortingState<TData> = ExtendedColumnSort<TData>[];
-
-export interface ExtendedColumnSort<TData> extends Omit<ColumnSort, "id"> {
-  id: StringKeyOf<TData>;
-}
-
-export type Prettify<T> = {
-  [K in keyof T]: T[K];
-} & {};
